9daa815bd8004f1f181987e8b0f6f97b6c58e03d
tandora <admin@tandora.co>
2019-06-07 08:34:14 +0530

    complete login, logout flow

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.urls import path
 from django.views.generic import TemplateView
 
-from frontend.views import auth
+from frontend.views import auth, app
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     url(r'^login', auth.login, name="frontend-login"),
+    url(r'^logout', auth.logout, name="frontend-logout"),
+    url(r'^app', app.index, name="frontend-staff-index"),
 ]
