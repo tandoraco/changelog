ff34ec5b1b027cf284c3db6792dfe530e51d2a2a
v-adhithyan <aavispeaks@gmail.com>
2019-06-09 09:32:52 +0530

    Add new changelog form
    Misc items
    1) add infinite scroll in list changelogs page and add ckeditor
    2) Add dependencies for ckeditor

--- frontend/views/auth/__init__.py
+++ frontend/views/auth/__init__.py
@@ -13,6 +13,9 @@ def login(request):
             user = User.objects.get(email=form.data['email'])
             token = str(uuid.uuid4())
             ClientToken.objects.create(token=token, user=user)
+            request.session["auth-token"] = token
+            request.session["email"] = user.email
+            request.session["user-id"] = user.id
             return render(request, 'login.html', {'token': token, 'is_logged_in': True, 'email': user.email})
     else:
         form = LoginForm()
