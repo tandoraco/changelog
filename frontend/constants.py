a58e70f48d6d669ddd7949ebb7970853a0019295
tandora <admin@tandora.co>
2019-06-15 20:27:12 +0530

    Add edit changelog from frontend

---
+++ frontend/constants.py
@@ -0,0 +1,4 @@
+from django.utils.translation import ugettext as _
+
+CHANGELOG_CREATED_OR_EDITED_SUCCESSFULLY = _("Changelog {} success.")
+CHANGELOG_DOES_NOT_EXIST_ERROR = _("Changelog does not exist.")
