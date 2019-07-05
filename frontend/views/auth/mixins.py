85aad20a4c3ce726fd905f2f6096c2e446022085
tandora <admin@tandora.co>
2019-07-05 07:59:13 +0530

    Refactor check auth method and add LoginRequired mixin

--- frontend/views/auth/mixins.py
+++ frontend/views/auth/mixins.py
@@ -1,19 +1,13 @@
-import functools
-
+from django.contrib.auth.mixins import AccessMixin
 from django.http import HttpResponseRedirect
 
 from frontend.forms.auth.utils import is_valid_auth_token_and_email
 
 
-def check_auth(func):
+class LoginRequiredMixin(AccessMixin):
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        request = args[0]
-
+    def dispatch(self, request, *args, **kwargs):
         if not is_valid_auth_token_and_email(request):
             return HttpResponseRedirect("/login")
 
-        return func(*args, **kwargs)
-
-    return wrapper
+        return super().dispatch(request, *args, **kwargs)
