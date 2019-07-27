41999ef0d7c43ec17528f9b29f0197883e0e2fa6
tandora <admin@tandora.co>
2019-07-27 10:01:48 +0530

    TAR-2 -> Fix color field issue in add category

--- frontend/forms/categories/__init__.py
+++ frontend/forms/categories/__init__.py
@@ -1,18 +1,16 @@
+from colorful.forms import RGBColorField
 from django import forms
-from django.forms import TextInput
 
 from v1.categories.models import Category
 from v1.utils.validators import validate_color, validate_category_name
 
 
 class CategoryForm(forms.ModelForm):
+    color = RGBColorField()
 
     class Meta:
         model = Category
         fields = ['name', 'color']
-        widgets = {
-            'color': TextInput(attrs={'type': 'color'}),
-        }
 
     def clean_color(self):
         return validate_color(self.cleaned_data['color'], serializer=False)
