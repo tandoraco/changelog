44a321a93532a3aeb6190f966be7f26937eb5f7a
tandora <admin@tandora.co>
2019-08-03 19:13:05 +0530

    TAR-5 nginx initial setup

---
+++ commons/scripts/entrypoint.sh
@@ -0,0 +1,10 @@
+echo "Waiting for postgres..."
+
+while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+echo "Running db migrations"
+python manage.py migrate
