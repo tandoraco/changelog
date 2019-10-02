a46d6d45f23b2d22e54a81d95b690a8e0a08c860
v-adhithyan <aavispeaks@gmail.com>
2019-10-02 20:54:21 +0530

    Add missing migrations
    add wait for postgres code inside entrypoint

--- commons/scripts/entrypoint.sh
+++ commons/scripts/entrypoint.sh
@@ -1,4 +1,13 @@
-sh wait-for-db.sh
+echo "Check for postgres .."
+
+until psql -h "tandora-backend_db_1" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres not started - sleeping"
+  sleep 5
+done
+
+
+echo "PostgreSQL started"
+sleep 2
 
 echo "Running db migrations"
 python manage.py migrate
