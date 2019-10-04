7ab81c903ae5c8fc370b7de66bf80b96ed2b5017
tandora <admin@tandora.co>
2019-10-04 21:51:34 +0530

    Remove postgres image from production docker
    1) Show appropriate validation if a deleted category name is used in create/edit category form
    2) Change str representation of embed model

--- v1/categories/constants.py
+++ v1/categories/constants.py
@@ -3,3 +3,4 @@ from django.utils.translation import ugettext as _
 
 INVALID_HEX_CODE = _("The color code is invalid.")
 CATEGORY_EXISTS = _("Category exists. Please enter a different name for category.")
+DELETED_CATEGORY = _('You cannot name a category with a deleted category name.')
