a3b52bb698e22897ab93e95608ec3f08e2688879
v-adhithyan <aavispeaks@gmail.com>
2019-10-30 08:22:24 +0530

    Add trial account column to company and related business logic

--- frontend/views/auth/mixins.py
+++ frontend/views/auth/mixins.py
@@ -1,12 +1,18 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import AccessMixin
 
+from frontend.constants import FREE_TRIAL_EXPIRED
 from frontend.custom.utils import redirect_to_login
-from frontend.forms.auth.utils import is_valid_auth_token_and_email
+from frontend.forms.auth.utils import is_valid_auth_token_and_email, is_trial_expired
 
 
 class LoginRequiredMixin(AccessMixin):
 
     def dispatch(self, request, *args, **kwargs):
+        if is_trial_expired(request):
+            messages.error(request, message=FREE_TRIAL_EXPIRED)
+            return redirect_to_login(request)
+
         if not is_valid_auth_token_and_email(request):
             return redirect_to_login(request)
 
