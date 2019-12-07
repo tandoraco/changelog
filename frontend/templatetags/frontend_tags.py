33b0b45afc0c92b565ff3c61905fb46e242448fe
v-adhithyan <aavispeaks@gmail.com>
2019-12-07 08:21:48 +0530

    Extract text from changelog content html and truncate in public index

--- frontend/templatetags/frontend_tags.py
+++ frontend/templatetags/frontend_tags.py
@@ -1,4 +1,7 @@
+from html.parser import HTMLParser
+
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
@@ -7,3 +10,19 @@ register = template.Library()
 def public_page_url(context):
     request = context['request']
     return request.session["public-page-url"]
+
+
+@register.filter
+@stringfilter
+def extract_text_and_truncate(value):
+
+    class HTML2Text(HTMLParser):
+        text = ""
+
+        def handle_data(self, data):
+            self.text += data
+
+    html_2_text = HTML2Text()
+    html_2_text.feed(value)
+
+    return html_2_text.text.strip()[:280]
