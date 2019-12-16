b2ce1f98ac536e51bc39076609162eae9c76f3db
v-adhithyan <aavispeaks@gmail.com>
2019-12-16 08:35:29 +0530

    Fix tests in v1 and move static site models to appropriate dir

--- v1/categories/views.py
+++ v1/categories/views.py
@@ -1,16 +1,12 @@
-from knox.auth import TokenAuthentication
-from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
 
 from v1.categories.models import Category
 from v1.categories.serializers import CategorySerializer
+from v1.utils.viewsets import TandoraModelViewSet
 
 ValidationError.status_code = 422
 
 
-class CategoriesViewSet(viewsets.ModelViewSet):
-    authentication_classes = (TokenAuthentication,)
+class CategoriesViewSet(TandoraModelViewSet):
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-    permission_classes = (IsAuthenticated,)
+    model_class = Category
