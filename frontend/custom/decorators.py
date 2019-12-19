88004d9aa9a8ce98356151968cd7d430a87cebf6
v-adhithyan <aavispeaks@gmail.com>
2019-12-19 09:03:50 +0530

    Add unit tests for static website

--- frontend/custom/decorators.py
+++ frontend/custom/decorators.py
@@ -37,7 +37,7 @@ def requires_static_site(func):
 
         if not company.is_static_site:
             messages.warning(request, NOT_ALLOWED)
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/staff/changelogs')
 
         return func(*args, **kwargs)
 
