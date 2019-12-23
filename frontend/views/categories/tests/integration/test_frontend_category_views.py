6df22270fb1116236541d453a0908f0a5f07081a
v-adhithyan <aavispeaks@gmail.com>
2019-12-23 09:40:26 +0530

    Add frontend changelog tests

--- frontend/views/categories/tests/integration/test_frontend_category_views.py
+++ frontend/views/categories/tests/integration/test_frontend_category_views.py
@@ -1,8 +1,7 @@
 import pytest
 from django.urls import reverse
-from rest_framework import status
 
-from frontend.custom.test_utils import TandoraTestClient
+from frontend.custom.test_utils import FrontEndFormViewTestBase, test_url
 
 VIEW_CATEGORIES = 'frontend-view-categories'
 CREATE_CATEGORY = 'frontend-create-category'
@@ -12,63 +11,22 @@ DELETE_CATEGORY = 'frontend-delete-category'
 
 @pytest.mark.django_db
 class TestFrontEndCategoryViews:
-    client = TandoraTestClient()
 
-    def test_view_categories(self, company, user, categories):
-        url = reverse(VIEW_CATEGORIES)
-        self.client.force_login(user)
+    def test_category_frontend_views(self, user, company, categories, category):
+        urls = [
+            test_url('create', reverse(CREATE_CATEGORY)),
+            test_url('edit', reverse(EDIT_CATEGORY, kwargs={'id': category.id})),
+            test_url('delete', reverse(DELETE_CATEGORY, kwargs={'id': category.id})),
+            test_url('view', reverse(VIEW_CATEGORIES)),
+        ]
 
-        response = self.client.get(url)
-        assert response.status_code == status.HTTP_200_OK
-        response_content = response.content.decode()
-        for category in categories:
-            assert category.name in response_content
-
-    def test_delete_category(self, company, user, category):
-        assert not category.deleted
-
-        url = reverse(DELETE_CATEGORY, kwargs={'id': category.id})
-        self.client.force_login(user)
-        response = self.client.get(url)
-        # on success we will be redirected to some other page
-        assert response.status_code == status.HTTP_302_FOUND
-        assert response.url == reverse(VIEW_CATEGORIES)
-
-        category.refresh_from_db()
-        assert category.deleted
-
-        response = self.client.get(url)
-        assert response.status_code == status.HTTP_302_FOUND
-
-    def test_create_category(self, company, user):
-        url = reverse(CREATE_CATEGORY)
-        self.client.force_login(user)
-        response = self.client.get(url)
-        assert response.status_code == status.HTTP_200_OK
-        response_content = response.content.decode().lower()
-        assert 'create category' in response_content
-
-    def test_edit_category(self, company, user, category):
-        url = reverse(EDIT_CATEGORY, kwargs={'id': category.id})
-        self.client.force_login(user)
-        response = self.client.get(url)
-        assert response.status_code == status.HTTP_200_OK
-        response_content = response.content.decode().lower()
-        assert 'edit category' in response_content
-        assert category.name.lower() in response_content
-        assert category.color.lower() in response_content
-
-        # deleted category editing should return 404
-        category.deleted = True
-        category.save()
-        category.refresh_from_db()
-        url = reverse(EDIT_CATEGORY, kwargs={'id': category.id})
-        self.client.force_login(user)
-        response = self.client.get(url)
-        assert response.status_code == status.HTTP_404_NOT_FOUND
-
-        # unexisting category editing should return 404
-        url = reverse(EDIT_CATEGORY, kwargs={'id': 100})
-        self.client.force_login(user)
-        response = self.client.get(url)
-        assert response.status_code == status.HTTP_404_NOT_FOUND
+        FrontEndFormViewTestBase(
+            model_name='Category',
+            urls=urls,
+            fields=['name', 'color'],
+            view_exclude_fields={'color'},
+            user=user,
+            company=company,
+            queryset=categories,
+            instance=category
+        )
