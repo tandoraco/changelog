f472a7cc6ff94a895bbfba6475a93119b87987ec
tandora <admin@tandora.co>
2019-12-24 10:56:06 +0530

    Add unit tests for forgot and reset password

--- v1/accounts/constants.py
+++ v1/accounts/constants.py
@@ -10,7 +10,7 @@ EMAIL_NOT_FOUND_ERROR = _("Email id is invalid.")
 PASSWORD_INCORRECT_ERROR = _("Incorrect password.")
 PASSWORD_LENGTH_VALIDATION_FAILED = _("Password length should be between {}-{} characters.".format(
     MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH))
-PASSWORD_CONSTRAINS_NOT_MET = _("""Password should contain atleast one uppercase, one lowercase,
+PASSWORD_CONSTRAINTS_NOT_MET = _("""Password should contain atleast one uppercase, one lowercase,
  one digit and one special character.""")
 EMAIL_OR_PASSWORD_INVALID = _("Email or Password is invalid.")
 SPECIAL_CHARACTERS_NOT_ALLOWED = _('Special characters are not allowed for this field.')
