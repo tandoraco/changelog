cb8f09613d2c69f6fdd0d3c70ba8c0b1ddfc2bec
tandora <admin@tandora.co>
2020-01-26 20:21:22 +0530

    Add image attachments api and enable image uploads in tiny mce

--- v1/core/urls.py
+++ v1/core/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from v1.core.views import ChangelogViewSet
+from v1.core.views import ChangelogViewSet, inline_image_attachment
 
 router = DefaultRouter()
 router.register(r'changelogs', ChangelogViewSet, basename='changelogs')
 
-urlpatterns = router.urls + []
+urlpatterns = router.urls + [
+    url(r'inline-image/', inline_image_attachment, name='v1-inline-image'),
+]
