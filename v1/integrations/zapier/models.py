b07b337b7f68a8633ddbeacfa3716e4d830a31e6
v-adhithyan <aavispeaks@gmail.com>
2020-02-09 18:59:33 +0530

    Complete zapier integration initial version

--- v1/integrations/zapier/models.py
+++ v1/integrations/zapier/models.py
@@ -5,8 +5,9 @@ from v1.utils import random_uuid
 
 class Zapier(models.Model):
     active = models.BooleanField(default=False)
-    api_key = models.UUIDField(default=random_uuid)
+    api_key = models.UUIDField(default=random_uuid, db_index=True)
     company = models.OneToOneField('Company', on_delete=models.CASCADE)
+    zapier_webhook_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return f'Zapier integration -> company {self.company}, active: {self.active}'
