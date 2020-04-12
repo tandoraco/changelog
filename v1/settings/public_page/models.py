1b46e975b081832bb43952a3378766dc43f81b9e
tandora <admin@tandora.co>
2020-04-12 11:25:12 +0530

    add public change log page customization
    Misc changes:
    1) fix color picker not displaying chosen color in staff forms

--- v1/settings/public_page/models.py
+++ v1/settings/public_page/models.py
@@ -3,15 +3,20 @@ from django.db import models
 
 from frontend.custom.utils import SettingsMixin
 
-
 DEFAULT_BLUE_COLOR = '#428bca'
+WHITE_COLOR = '#FFFFFF'
 
 
 class PublicPage(SettingsMixin, models.Model):
     company = models.OneToOneField('Company', null=False, on_delete=models.CASCADE)
-    color = RGBColorField(default=DEFAULT_BLUE_COLOR)
-    hide_from_crawlers = models.BooleanField(default=True)
+    banner_color = RGBColorField(default=DEFAULT_BLUE_COLOR)
+    banner_font_color = RGBColorField(default=WHITE_COLOR)
+    font = models.TextField(blank=True, null=True)
+    logo = models.ImageField(null=True, blank=True)
     _settings = models.TextField(blank=True, null=True, db_column='settings')
+    hide_from_crawlers = models.BooleanField(default=True,
+                                             help_text='Check this option, '
+                                                       'if you don\'t want your page to be indexed by search engines.')
 
     def __str__(self):
         return f"{self.company}"
