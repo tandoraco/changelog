3d823a5adde317a72920454be253e3f8bd06a1be
tandora <admin@tandora.co>
2020-04-19 21:24:52 +0530

    fix pytest

--- v1/widget/tests/unit/test_widget_serializers.py
+++ v1/widget/tests/unit/test_widget_serializers.py
@@ -11,11 +11,8 @@ class TestWidgetSerializer(SerializerTestBase):
 
     def test_embed_serializer(self, company):
         data = [
-            SerializerTestData(data={'company': company.id, 'color': 'asdfdfdf'}, is_valid=False),
-            SerializerTestData(data={'company': company.id, 'color': '#000000'}, is_valid=True),
-            SerializerTestData(data={'company': company.id, 'color': '000000'}, is_valid=True),
             SerializerTestData(data={'company': company.id,
-                                     'color': '000000', 'javascript': '<script></script>', 'css': ''}, is_valid=True)
+                                     'javascript': '<script></script>', 'css': ''}, is_valid=True)
         ]
 
         self.run_data_assertions(data)
