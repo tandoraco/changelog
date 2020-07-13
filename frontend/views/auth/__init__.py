b333d7a7d5ca0fdde8becf5ebecff23cba49d179
tandora <admin@tandora.co>
2020-07-13 08:30:51 +0530

    .

--- frontend/views/auth/__init__.py
+++ frontend/views/auth/__init__.py
@@ -3,7 +3,6 @@ from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
 
@@ -114,7 +113,7 @@ def company_form(request):
         .get_form(request, success_message=COMPANY_CREATED_OR_EDITED_SUCCESSFULLY,
                   error_message=COMPANY_DOES_NOT_EXIST, id=id)
 
-@csrf_exempt
+
 @api_view(['POST'])
 @authentication_classes([])
 @transaction.atomic
