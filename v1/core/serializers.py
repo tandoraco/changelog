4384289f1e5148cecf38a3cc1a770804c9b5f904
tandora <admin@tandora.co>
2020-07-21 10:52:22 +0530

    add created and last edited to polling data of zapier

--- v1/core/serializers.py
+++ v1/core/serializers.py
@@ -35,7 +35,9 @@ class ChangelogSerializerForZapier(ChangelogSerializer):
             'content_text': html_2_text(instance.content),
             'images': all_images,
             'random_image': random.choice(all_images) if all_images else '',
-            'view_url': f'{settings.HOST}{company_name}/{terminology}/{instance.slug}'
+            'view_url': f'{settings.HOST}{company_name}/{terminology}/{instance.slug}',
+            'created_at': instance.created_at,
+            'last_edited_at': instance.last_edited_at
         }
 
 
