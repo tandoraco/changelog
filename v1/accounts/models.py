1f273c23f244db2e842ead824b30b8fedf36db73
tandora <admin@tandora.co>
2020-08-17 17:41:22 +0530

    notify new company signup in slack

--- v1/accounts/models.py
+++ v1/accounts/models.py
@@ -8,7 +8,7 @@ from django.db.models.signals import post_save
 from django.utils.text import slugify
 
 from v1.accounts.constants import CHANGELOG_TERMINOLOGY, MAX_EMAIL_LENGTH, USE_CASE_CHOICES
-from v1.accounts.signals import post_new_affiliate_signup_to_slack
+from v1.accounts.signals import post_new_affiliate_signup_to_slack, notify_new_company_signup_in_slack
 from v1.accounts.utils import UserManager
 from v1.notifications import email as notification_email
 from v1.utils import prettify_json, random_uuid
@@ -256,3 +256,4 @@ class CustomDomain(models.Model):
 post_save.connect(notification_email.send_forgot_password_mail, sender=ForgotPassword)
 post_save.connect(notification_email.send_user_verification_email, sender=User)
 post_save.connect(post_new_affiliate_signup_to_slack, sender=Affiliate)
+post_save.connect(notify_new_company_signup_in_slack, sender=Company)
