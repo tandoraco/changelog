e1441ab0040471f67ab0003c5caaf042f5dac597
tandora <admin@tandora.co>
2020-10-25 10:26:15 +0530

    display pending invoice data in billing page

--- frontend/views/billing/__init__.py
+++ frontend/views/billing/__init__.py
@@ -6,6 +6,7 @@ from django.apps import apps
 from django.shortcuts import render
 
 from frontend.custom.decorators import is_admin, is_authenticated
+from v1.accounts.models import PendingInvoice
 
 DAYS_IN_A_YEAR = 365
 
@@ -42,6 +43,16 @@ def billing_page(request):
             'invoice': invoice
         }
 
+        try:
+            pending_invoice = PendingInvoice.objects.get(company=company)
+            pending_invoice_data = {
+                'pending_invoice': True,
+                'pending_invoice_url': pending_invoice.short_url,
+                'pending_invoice_expiry_time': pending_invoice.expiry_time
+            }
+            context.update(**pending_invoice_data)
+        except PendingInvoice.DoesNotExist:
+            pass
     except apps.get_model('v1', 'Subscription').DoesNotExist:
         context = {key: '~' for key in billing_keys}
 
