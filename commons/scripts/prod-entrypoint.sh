10904b7b34c1b3cbe6a174152659028fb093fe2b
tandora <admin@tandora.co>
2021-01-31 12:37:26 +0530

    add newrelic to production

--- commons/scripts/prod-entrypoint.sh
+++ commons/scripts/prod-entrypoint.sh
@@ -5,6 +5,9 @@ pip install -r requirements_dev.txt
 echo "Running db migrations"
 python manage.py migrate
 
+pip install newrelic
+export NEW_RELIC_CONFIG_FILE=newrelic.ini
+newrelic-admin run-program
 exec gunicorn tandora.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2 \
