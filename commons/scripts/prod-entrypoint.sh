251c03baa208f3d1506dc9b74aba6b50176fffdc
tandora <admin@tandora.co>
2021-02-01 12:45:53 +0530

    new relic changes

--- commons/scripts/prod-entrypoint.sh
+++ commons/scripts/prod-entrypoint.sh
@@ -6,8 +6,7 @@ echo "Running db migrations"
 python manage.py migrate
 
 pip install newrelic
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program
-newrelic-admin run-program
+newrelic-admin run-program newrelic.ini
 exec gunicorn tandora.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2 \
