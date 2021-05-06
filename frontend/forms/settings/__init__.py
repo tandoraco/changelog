d6b07d23bf3d96b1680e01c6b47b8a5b7fc46165
tandora <admin@tandora.co>
2021-05-06 18:24:24 +0530

    fix widget and public page

--- frontend/forms/settings/__init__.py
+++ frontend/forms/settings/__init__.py
@@ -2,11 +2,9 @@ from django import forms
 from django.apps import apps
 
 from frontend.constants import PUBLIC_PAGE_CHANGELOG_10_MULTIPLE_ERROR
-from frontend.forms.static_site import FONT_CHOICES
 
 
 class PublicPageForm(forms.ModelForm):
-    font = forms.ChoiceField(choices=FONT_CHOICES, required=True)
 
     def clean_changelog_limit(self):
         changelog_limit = self.cleaned_data.get('changelog_limit')
@@ -16,4 +14,4 @@ class PublicPageForm(forms.ModelForm):
 
     class Meta:
         model = apps.get_model('v1', 'PublicPage')
-        exclude = ('company', '_settings', )
+        exclude = ('company', '_settings', 'font', )
