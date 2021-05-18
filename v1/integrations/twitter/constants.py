b8af5c69c5e8995f48d4f98248fdf8e5ccf28e3b
tandora <admin@tandora.co>
2021-05-18 18:26:36 +0530

    add initial twitter models, serializers and handlers

---
+++ v1/integrations/twitter/constants.py
@@ -0,0 +1,9 @@
+DEFAULT_TWEET_CONTENT = '''
+Modify this tweet as per your requirement.
+You can use the following variables that will reference the changelog.
+{title}
+{content}
+{category}
+{link}
+Please use the above variables in your tweet content which will get substituted with the changelog values.
+'''
