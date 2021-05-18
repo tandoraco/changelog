819285e901e0f2f12a16220282beeddd01bbd7b3
tandora <admin@tandora.co>
2021-05-18 22:19:49 +0530

    handle twitter background job

--- v1/integrations/twitter/constants.py
+++ v1/integrations/twitter/constants.py
@@ -1,3 +1,11 @@
+TWEET_SUBSTITUTES_MAP = {
+    '{title}': 'get_title',
+    '{content}': 'get_content',
+    '{category}': 'get_category',
+    '{link}': 'get_link',
+}
+
+
 DEFAULT_TWEET_CONTENT = '''
 Modify this tweet as per your requirement.
 You can use the following variables that will reference the changelog.
--- v1/integrations/twitter/handlers.py
+++ v1/integrations/twitter/handlers.py
@@ -1,4 +1,11 @@
-from v1.integrations.handlers import IntegrationHandlerBase, IntegrationSettingsHandlerBase
+from urllib.parse import urljoin
+
+from django.conf import settings
+from django.utils.text import slugify
+
+from v1.integrations.handlers import IntegrationHandlerBase, IntegrationSettingsHandlerBase, BackgroundJobHandlerBase
+from v1.integrations.twitter.client import TwitterClient
+from v1.integrations.twitter.constants import TWEET_SUBSTITUTES_MAP
 from v1.integrations.twitter.models import Twitter
 from v1.integrations.twitter.serializers import TwitterSerializer
 
@@ -19,3 +26,34 @@ class TwitterSettingsHandler(IntegrationSettingsHandlerBase):
 
 class TwitterHandler(IntegrationHandlerBase):
     pass
+
+
+class TwitterBackgroundJobHandler(BackgroundJobHandlerBase):
+
+    def get_title(self):
+        return self.changelog.title
+
+    def get_content(self):
+        return self.changelog.content
+
+    def get_category(self):
+        return self.changelog.category.name
+
+    def get_link(self):
+        link = urljoin(settings.HOST, f'{slugify(self.company.company_name)}/{self.changelog.slug}')
+        return link
+
+    def substitute_tweet(self):
+        tweet_content = self.integration_object.tweet_content
+        for attr_name, func in TWEET_SUBSTITUTES_MAP.items():
+            tweet_content = tweet_content.replace(attr_name, getattr(self, func)())
+        return tweet_content
+
+    def execute(self, **kwargs):
+        tweet = self.substitute_tweet()
+
+        twitter_client = TwitterClient(self.integration_object)
+        if kwargs.get('created') and self.integration_object.tweet_when_created:
+            twitter_client.send_to_twitter(tweet)
+        elif self.integration_object.tweet_when_published:
+            twitter_client.send_to_twitter(tweet)
