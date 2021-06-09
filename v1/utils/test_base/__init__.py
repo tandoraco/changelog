c622215a633f7f3f83f029b726b5a201c85bebc2
tandora <admin@tandora.co>
2021-06-09 08:24:49 +0530

    fix failing tests in ci and add twitter fixtures

--- v1/utils/test_base/__init__.py
+++ v1/utils/test_base/__init__.py
@@ -2,3 +2,14 @@ from collections import namedtuple
 
 SerializerTestData = namedtuple('TestSerializerData', 'data is_valid')
 AssertionErrors = namedtuple('AssertionErrors', 'key value')
+
+
+class TestResponse:
+
+    def __init__(self, status_code, content):
+        self.status_code = status_code
+        self.content = content
+
+    @property
+    def ok(self):
+        return 200 <= self.status_code < 300
