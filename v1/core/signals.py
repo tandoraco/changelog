3abe5414dd922c6ed2eec85225d3f73f4c2f9e1c
tandora <admin@tandora.co>
2021-10-27 16:39:33 +0530

    fix tests

--- v1/core/signals.py
+++ v1/core/signals.py
@@ -9,16 +9,22 @@ def get_or_populate_slug_field(sender, instance, *args, **kwargs):
 
 
 def update_pinned_changelog(sender, instance, *args, **kwargs):
-    if instance.deleted and instance.company.pinnedchangelog.changelog:
-        pinned_changelog = instance.company.pinnedchangelog
-        if pinned_changelog.changelog.id == instance.id:
-            pinned_changelog.changelog = None
-            pinned_changelog.save()
+    try:
+        if instance.deleted and instance.company.pinnedchangelog.changelog:
+            pinned_changelog = instance.company.pinnedchangelog
+            if pinned_changelog.changelog.id == instance.id:
+                pinned_changelog.changelog = None
+                pinned_changelog.save()
+    except Exception:
+        pass
 
 
 def remove_pinned_changelog(sender, instance, *args, **kwargs):
-    if instance.company.pinnedchangelog.changelog:
-        pinned_changelog = instance.company.pinnedchangelog
-        if pinned_changelog.changelog.id == instance.id:
-            pinned_changelog.changelog = None
-            pinned_changelog.save()
+    try:
+        if instance.company.pinnedchangelog.changelog:
+            pinned_changelog = instance.company.pinnedchangelog
+            if pinned_changelog.changelog.id == instance.id:
+                pinned_changelog.changelog = None
+                pinned_changelog.save()
+    except Exception:
+        pass
